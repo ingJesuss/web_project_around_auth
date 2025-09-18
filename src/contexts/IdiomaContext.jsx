c4ee94ef16fed01db8fb25aabfd961dfed4e639a
Jesus Hernandez Lopez <ing_jesushl@hotmail.com>
2025-09-18 13:49:20 -0600

    reestructuracion de proyecto

--- src/contexts/IdiomaContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const IdiomaContext = createContext();
-
-export default IdiomaContext
--- src/contexts/TemaContext.jsx
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const TemaContext = createContext();
-
-export default TemaContext;
